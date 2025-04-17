679df38a886701ad31c58c9819c4c90b8fc3500e
VinnyMendess <vinimendesdasilvaa@gmail.com>
2025-04-16 21:21:28 -0300

    adicionando contador de tarefas concluidas e checkbox

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+let contador = 0;
+let contadorConcluido = 0;
+
 function mostraTexto(){
 
   const texto = document.getElementById('entradaTarefa').value
@@ -6,8 +9,58 @@ function mostraTexto(){
   if (texto.trim() !== "") {
     const novaMensage = document.createElement('div');
     novaMensage.classList.add('mensagem');
-    novaMensage.textContent = texto;
+    
 
-    document.getElementById('exibicaoTexto').appendChild(novaMensage)
+    const textoSpan = document.createElement('span');
+    textoSpan.textContent = texto;
+    
+
+    const lixeira = document.createElement('div');
+    lixeira.classList.add('lixeira');
+    lixeira.innerHTML = '<i class="fa-solid fa-trash"></i>';
+
+    const caixaStatus = document.createElement('div');
+    caixaStatus.classList.add('caixaStatus')
+
+    caixaStatus.onclick = function () {
+      caixaStatus.classList.toggle('ativo')
+  
+      if (caixaStatus.classList.contains('ativo')) {
+        contadorConcluido++;
+        atualizaContadorConcluido();
+        } else {
+          contadorConcluido--;
+          atualizaContadorConcluido();
+        }
+    }
+
+    
+    lixeira.onclick = function () {
+      novaMensage.remove();
+      contador--;
+      atualizaContador();
+      contadorConcluido--;
+      atualizaContadorConcluido();
+    }
+
+    
+    novaMensage.appendChild(caixaStatus);
+    novaMensage.appendChild(textoSpan);
+    novaMensage.appendChild(lixeira);
+    document.getElementById('exibicaoTexto').appendChild(novaMensage);
+
+    contador++;
+    atualizaContador();
+  
   }
 }
+
+function atualizaContador(){
+  document.getElementById('contadorTarefas').textContent = contador;
+}
+function atualizaContadorConcluido(){
+  document.getElementById('contadorConcluido').textContent = contadorConcluido;
+}
+
+
+
